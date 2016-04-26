6d681fa6f6faa28dc12ca654c9562aebde584590
AGolumbovsky <agolumbovsky@gmail.com>
2016-04-25 21:52:26 -0400

    shaping up

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,22 @@
+$(document).ready(function () {
 
-$(document).ready(function() {
+   var location = window.location;
+   var found = false;
+   $("#tab-container a").each(function(){
+      var href = $(this).attr("href");
+      if(href==location){
+         $(this).parent().addClass("selected");
+         found = true;
+      }
+   });
 
-	var testo = $('#WebDev');
+   if(!found){
+      $("#tab-container li:first").addClass("selected");
+   }
 
-	testo.click(function() {
-		testo.hide('slow');
-	});
+
+   $("#tab-container li").click(function() {
+      $("#main-container").append("<h1>fuckity fuckity fuck perefuck</h1>");
+   });
 
 });
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var app = express();
 
 var port = 8778;
 
-//server static files
+//serve static files
 app.use(express.static(__dirname + '/public'));
 
+// time stamp for nodemon
+var now = new Date().getTime();
 
-
+// listen carefully
 app.listen(process.env.PORT || port, function() {
-	console.log("running on node, port " + port);
+	console.log("running on node port " + port + ' ' + now);
 });
